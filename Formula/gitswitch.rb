89fd36a84f8b5d5f87c49b7caf00ba2eb17f4f2f
aksisonline <teamscientify2016@gmail.com>
2026-04-20 23:37:09 +0530

    Add gitswitch formula

---
+++ Formula/gitswitch.rb
@@ -0,0 +1,38 @@
+class Gitswitch < Formula
+  desc "Terminal UI for managing multiple local git identities"
+  homepage "https://github.com/aksisonline/gitswitch"
+  version "0.1.6"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/aksisonline/gitswitch/releases/download/v#{version}/gitswitch-darwin-arm64"
+      sha256 "REPLACE_WITH_ARM64_SHA256"
+    else
+      url "https://github.com/aksisonline/gitswitch/releases/download/v#{version}/gitswitch-darwin-amd64"
+      sha256 "REPLACE_WITH_AMD64_SHA256"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/aksisonline/gitswitch/releases/download/v#{version}/gitswitch-linux-arm64"
+      sha256 "REPLACE_WITH_LINUX_ARM64_SHA256"
+    else
+      url "https://github.com/aksisonline/gitswitch/releases/download/v#{version}/gitswitch-linux-amd64"
+      sha256 "REPLACE_WITH_LINUX_AMD64_SHA256"
+    end
+  end
+
+  def install
+    binary_name = if OS.mac?
+                    "gitswitch-darwin-#{Hardware::CPU.arm? ? 'arm64' : 'amd64'}"
+                  else
+                    "gitswitch-linux-#{Hardware::CPU.arm? ? 'arm64' : 'amd64'}"
+                  end
+    bin.install binary_name => "gitswitch"
+  end
+
+  test do
+    system "#{bin}/gitswitch", "--help"
+  end
+end
